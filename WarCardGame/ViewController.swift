b597db79fe259795fb7f0c91712c10463e0c4897
chenzhizs <chenzhi@qq.com>
2021-04-05 12:49:49 +0900

    https://www.youtube.com/watch?v=Afp-qjZHZvs&list=PLMRqhzcHGw1ZkH8RuznGMS0NZs0jWQQ5a&index=10

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LeftImageView: UIImageView!
+    
+    @IBOutlet weak var RightImageView: UIImageView!
+    
+    @IBOutlet weak var LeftScoreLabel: UILabel!
+    
+    @IBOutlet weak var RightScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftNumber = Int.random(in: 1...13)
+        let rightNumber = Int.random(in: 1...13)
+        
+        LeftImageView.image = UIImage(named: "\(leftNumber)")
+        
+        RightImageView.image = UIImage(named: "\(rightNumber)")
+        
+    }
+    
 }
 
