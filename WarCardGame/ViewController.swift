111c1c780df22a79fa862d1a8ac07eff5670fe97
chenzhizs <chenzhi@qq.com>
2021-04-05 13:05:28 +0900

    https://www.youtube.com/watch?v=Afp-qjZHZvs&list=PLMRqhzcHGw1ZkH8RuznGMS0NZs0jWQQ5a&index=11

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -23,14 +23,31 @@ class ViewController: UIViewController {
         
         
     }
+    
+    var leftScore = 0
+    var rightScore = 0
+    
 
     @IBAction func dealTapped(_ sender: Any) {
-        let leftNumber = Int.random(in: 1...13)
-        let rightNumber = Int.random(in: 1...13)
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
         
         LeftImageView.image = UIImage(named: "\(leftNumber)")
         
         RightImageView.image = UIImage(named: "\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            
+            leftScore += 1
+            LeftScoreLabel.text = String(leftScore)
+            
+        }else if leftNumber < rightNumber {
+            
+            rightScore += 1
+            RightScoreLabel.text = String(rightScore)
+        }else{
+            
+        }
         
     }
     
